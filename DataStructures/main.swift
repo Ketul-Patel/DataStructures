f14f5aeb0784e116116cd6668c02f40b2a83e373
Ketul Patel <kjpatel2010@gmail.com>
2015-04-26 22:16:56 -0700

    Initial Commit

---
+++ DataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataStructures
+//
+//  Created by Ketul Patel on 4/26/15.
+//  Copyright (c) 2015 Ketul Patel. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
